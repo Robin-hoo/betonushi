2ee2a02c34f85077e973eaf2f69637dae6a443b9
Nguyen Duc Manh <167436213+manhnd52@users.noreply.github.com>
2025-11-26 09:19:27 +0700

    Revert "feature:food-detail"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainLayout from "@/layouts/MainLayout";
-import AuthLayout from "@/layouts/AuthLayout";
-import HomePage from "@/pages/HomePage";
-import FoodDetailPage from "./pages/FoodDetailPage";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainLayout from '@/layouts/MainLayout';
+import AuthLayout from '@/layouts/AuthLayout';
+import HomePage from '@/pages/HomePage';
 
 function App() {
   return (
@@ -10,26 +9,11 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
-          <Route path="/food" element={<HomePage />} />
-          <Route path="/food/:id" element={<FoodDetailPage />} />
-          <Route path="/restaurant" element={<HomePage />} />
-          <Route path="/phrases" element={<HomePage />} />
-          <Route
-            path="/about"
-            element={<div className="p-4">About Page</div>}
-          />
+          <Route path="/about" element={<div className="p-4">About Page</div>} />
         </Route>
         <Route element={<AuthLayout />}>
-          <Route
-            path="/login"
-            element={<div className="text-center">Login Form Placeholder</div>}
-          />
-          <Route
-            path="/register"
-            element={
-              <div className="text-center">Register Form Placeholder</div>
-            }
-          />
+          <Route path="/login" element={<div className="text-center">Login Form Placeholder</div>} />
+          <Route path="/register" element={<div className="text-center">Register Form Placeholder</div>} />
         </Route>
       </Routes>
     </Router>
