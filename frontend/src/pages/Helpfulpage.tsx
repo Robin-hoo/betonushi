d798dc46e1af6fcebdb2c1f5966664f6719bdd65
HoanxHoan <146335893+HoanxHoan@users.noreply.github.com>
2025-11-26 10:22:13 +0700

    translate

--- frontend/src/pages/Helpfulpage.tsx
+++ frontend/src/pages/Helpfulpage.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
+
 export default function Helpfulpage() {
         const [edit3, setEdit3] = useState(false);
         const [text3, setText3] = useState(
@@ -6,14 +9,14 @@ export default function Helpfulpage() {
         Bさん:「う、好き！ 特にフォー！」
         (DI)「B-san、よトナム料理ど？」「け、後一緒行う！」`
     );
-
+  const { t } = useTranslation();
   return (
     <div className="w-full flex flex-col items-center p-6 bg-gray-50 min-h-screen">
 
       {/* Block 1 - Title */}
       <div className="w-full max-w-3xl text-center border p-6 bg-white rounded-xl shadow-sm mb-8">
-        <h1 className="text-3xl font-bold mb-2">ベトナム的料理実施ガイド</h1>
-        <p className="text-gray-600 text-sm">Helpful Japanese Phrases for Food & Invitations</p>
+        <h1 className="text-3xl font-bold mb-2">{t("block1.title")}</h1>
+        <p className="text-gray-600 text-sm">{t("block2.title")}</p>
       </div>
 
       {/* Block 2 - Content */}
@@ -23,7 +26,7 @@ export default function Helpfulpage() {
         <div className="bg-green-100 p-4 rounded-lg flex gap-4 items-start">
              <img src="/invite.jpg" alt="invite" className="w-35 h-35 " />
         <div className="bg-green-100 p-4 rounded-lg">
-          <h2 className="font-bold text-lg mb-2">1. 誘う (Inviting)</h2>
+          <h2 className="font-bold text-lg mb-2">{t("section1.title")}</h2>
           <p>友達、ベトナム的料理食べに行かない？</p>
           <p>美味しいフォーの店、知ってますよ！</p>
         </div>
@@ -32,7 +35,7 @@ export default function Helpfulpage() {
         <div className="bg-orange-100 p-4 rounded-lg flex gap-4 items-start">
              <img src="/asking.jpg" alt="invite" className="w-35 h-35 " />
         <div className="bg-orange-100 p-4 rounded-lg">
-          <h2 className="font-bold text-lg mb-2">2. 質問する (Asking Questions)</h2>
+          <h2 className="font-bold text-lg mb-2">{t("section2.title")}</h2>
           <p>牛肉の、何料理？</p>
           <p>パクチは好き？</p>
           <p>好きな家庭料理なのなに？</p>
@@ -41,7 +44,7 @@ export default function Helpfulpage() {
         {/* Section 3 */}
         {edit3 ? (
         <div className="bg-gray-300 p-4 rounded-lg">
-        <h2 className="font-bold text-lg mb-2">3. 会話例 (Sample Dialogue)</h2>
+        <h2 className="font-bold text-lg mb-2">{t("section3.title")}</h2>
 
         <textarea 
         className="w-full p-2 rounded" 
@@ -63,7 +66,7 @@ export default function Helpfulpage() {
     <img src="/sample.jpg" alt="sample" className="w-35 h-35" />
 
     <div className="flex-1">
-      <h2 className="font-bold text-lg mb-2">3. 会話例 (Sample Dialogue)</h2>
+      <h2 className="font-bold text-lg mb-2">{t("section3.title")}</h2>
 
       {/* HIỂN THỊ NỘI DUNG ĐÃ EDIT */}
       <pre className="whitespace-pre-wrap">{text3}</pre>
